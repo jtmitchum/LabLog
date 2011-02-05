97b36d9be9109a47ce7a447941a8bfcac928399c
JT Mitchum <jtmitchum@gmail.com>
2011-02-04 19:42:10 -0600

    updated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LabLog::Application.config.secret_token = 'fcbd9788dbd2ee07844ddbbf660905536730ba5903d11ba825e506a4b4f8c9fa4f3e4d0dc1ed19f41dfe76a677dbc564f125fb5469ffdf1c0cad20d958b1ec82'
